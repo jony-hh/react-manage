89669df2168c264e6ebff022e4309bdb7901c303
Jony <jonyh0812@gmail.com>
2024-02-01 00:37:28 +0800

    feat: 请求与登录的配置

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,48 @@
+import './index.scss'
+import {Card, Form, Input, Button} from 'antd'
+import logo from '@/assets/logo.png'
+
 const Login = () => {
-    return <div>this is login</div>
+    return (
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt=""/>
+                {/* 登录表单 */}
+                <Form validateTrigger={['onBlur']} onFinish={ onFinish }>
+                    <Form.Item
+                        name="mobile"
+                        rules={[
+                            { required: true, message: '请输入手机号' },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: '手机号码格式不对'
+                            }
+                        ]}
+                    >
+                        <Input size="large" placeholder="请输入手机号" />
+                    </Form.Item>
+                    <Form.Item
+                        name="code"
+                        rules={[
+                            { required: true, message: '请输入验证码' },
+                        ]}
+                    >
+                        <Input size="large" placeholder="请输入验证码" maxLength={6} />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
+}
+
+// 点击登录按钮时触发 参数values即是表单输入数据
+const onFinish = formValue => {
+    console.log(formValue)
 }
 export default Login
